176e6a11663beaa01f6236405d57a670dbf50e26
Sriram Nair <shnair1995@gmail.com>
2021-09-21 20:36:10 -0700

    Finished day 2 Lab

--- lab.js
+++ lab.js
@@ -155,11 +155,13 @@ else{
 
 //CODE HERE
 var changeMyMind = true 
-if(changeMyMind === true){
-  changeMyMind === false
+if(changeMyMind !== true){
+  changeMyMind = false
+  console.log(changeMyMind)
 }
-else if(changeMyMind === false){
-  changeMyMind === true
+else if(changeMyMind !== false){
+  changeMyMind = true
+  console.log(changeMyMind)
 }
 
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
@@ -178,15 +180,21 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 /*
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
-var someNum = myFavoriteNumbers[4]
+
 
 //Code Here
-
+var someNum = myFavoriteNumbers[4]
 
 
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
-if(myFavoriteNumbers.length < 7) 
+if(myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in this array')
+} 
+else if(myFavoriteNumbers.length > 7){
+  someNum = myFavoriteNumbers[6]
+  console.log(someNum)
+}
 //Code Here
 
 
@@ -199,7 +207,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for(let i = 0; i < listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log(listOfNumbers[i])
+  }
+} 
 
 ////////// PROBLEM 12 //////////
 // Do not edit the code below.
@@ -209,7 +221,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 /*
   Loop backwards, starting at the end of the 'letters' array. Console-log every item in the array.
 */
-
+for (i = letters.length - 1; i >= 0; i--){
+  console.log(letters[i])
+}
 //Code Here
 
 
@@ -224,6 +238,7 @@ let letterGrade = 'B'
 
 /* Use a switch statement on 'letterGrade' and console.log the appropriate response.
 
+
 If A: "The student is doing excellently."
 If B: "The student is doing well."
 If C: "The student is doing alright."
@@ -233,13 +248,29 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+/*switch(letterGrade){
+  case A:"The student is doing excellently"
+    console.log()
+    break
+  case B:"The student is doing well"
+    console.log()
+    break
+  case C:"The student is doing alright" 
+    console.log()
+    break
+  case D:"The student is not doing very well" 
+    console.log()
+    break
+  case F:"The student it failing"
+    console.log()
+    break 
+}
 
 
 ////////// PROBLEM 14 //////////
 /* The famous FizzBuzz, Devmountain style!
   Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
-
+  
   Your output should look like:
   1
   2
@@ -261,3 +292,17 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for(i = 1; i <= 100; i++){
+  if(i % 5 === 0 && i % 3 === 0){
+    console.log('Devmountain')
+  }
+  else if(i % 5 === 0){
+    console.log('Mountain')
+  }
+  else if(i % 3 === 0){
+    console.log('Dev')
+  }
+  else{
+    console.log(i)
+  }
+}
